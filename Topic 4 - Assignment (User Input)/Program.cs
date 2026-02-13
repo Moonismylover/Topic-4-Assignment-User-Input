2f9b7df26098f79d813de290833c276c3f2d3026
700014473 <700014473@2260I-L29-008>
2026-02-13 13:11:43 -0500

    Add project files.

---
+++ Topic 4 - Assignment (User Input)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_4___Assignment__User_Input_
+{
+    internal class Program
+    {
+        public static void Part1()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Topic 4 - Assignment (User Input)";
+
+        }
+    }
+}
