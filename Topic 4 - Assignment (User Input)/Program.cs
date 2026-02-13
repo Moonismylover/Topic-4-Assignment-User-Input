d652469276004d09e75dbdd00df010af8c89bdb2
700014473 <700014473@2260I-L29-008>
2026-02-13 13:21:58 -0500

    heyyyyyyyyyyyy

--- Topic 4 - Assignment (User Input)/Program.cs
+++ Topic 4 - Assignment (User Input)/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        // Angel 
+
         public static void Part1()
         {
-            
+            string name;
+            int age;
+            double hourlyWage;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("What is your name? \nEnter here: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "! Nice to meet you!");
+
+            Console.WriteLine(" ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("What is your age? \nEnter here: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Damn! You are " + age + " years old! That's ancient!");
+
+            Console.WriteLine(" ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("So since you are so old, you must have a job! How much do you make hourly? \nEnter here: ");
+            Double.TryParse(Console.ReadLine(), out hourlyWage);
+            Console.WriteLine("Wow! You make " + hourlyWage.ToString("C") + " an hour! That's great cause I make ZERO!");
+
+
+
         }
 
         static void Main(string[] args)
